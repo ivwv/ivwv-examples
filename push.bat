6c2dd7a33434e2d4029a0a39c1047721dc8d4dfc
sy <sy18959889840@gmail.com>
2023-02-10 14:45:11 +0800

    测试

--- push.bat
+++ push.bat
@@ -1,8 +1,6 @@
-
-echo "# ivwv-examples" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "测试"
 git branch -M main
 git remote add origin https://github.com/ivwv/ivwv-examples.git
 git push -u origin main
